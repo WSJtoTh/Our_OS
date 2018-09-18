3eceaaec6cbc52cda6da8ff354b25e98dd1e5831
WSJtoTh <450442610@qq.com>
2018-09-18 21:13:45 +0800

    hhh

---
+++ src/Device/Register.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package Device;
+
+/**
+ * @author 45044
+ *
+ */
+public class Register {
+	public static int responseINTRIDReg = -1;
+	public static DevType responseDevType = DevType.DEFAULT;
+}
